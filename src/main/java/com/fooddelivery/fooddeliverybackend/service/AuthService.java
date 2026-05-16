9def1729510b38396b2f9c47ba9a7100333a5005
Varad163 <varadgholap163@gmail.com>
2026-05-16 22:11:18 +0530

    hashed password

--- src/main/java/com/fooddelivery/fooddeliverybackend/service/AuthService.java
+++ src/main/java/com/fooddelivery/fooddeliverybackend/service/AuthService.java
@@ -5,6 +5,7 @@ import com.fooddelivery.fooddeliverybackend.entity.Role;
 import com.fooddelivery.fooddeliverybackend.entity.User;
 import com.fooddelivery.fooddeliverybackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,12 +14,19 @@ public class AuthService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public String signup(SignupRequest request) {
 
         User user = User.builder()
                 .name(request.getName())
                 .email(request.getEmail())
-                .password(request.getPassword())
+                .password(
+                        passwordEncoder.encode(
+                                request.getPassword()
+                        )
+                )
                 .role(Role.CUSTOMER)
                 .build();
 
